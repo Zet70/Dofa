272029d32a01371bce168581ddb492ae98eeac34
quetzo <hojitabrillante@gmail.com>
2022-12-22 09:38:37 -0600

    arreglando seeds y agregando nuevos datos a la tabla

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,9 @@
 class CharactersController < ApplicationController
   def index
     @first_character = Character.first # Character.new(datos...)
-    
+    @second_character = Character.second # Character.new(datos...)
+    @third_character = Character.third # Character.new(datos...)
+
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Character.create([ 
-  name: "zurcaritas",lvl: 5, characteristic: "hola", exp: 100,
-  name: "iop",lvl: 1, characteristic: "probando", exp: 0,
-  name: "steamer",lvl: 1, characteristic: "hola", exp: 0,
-  name: "timador",lvl: 6, characteristic: "hola", exp: 150,
-  name: "sram",lvl: 8, characteristic: "hola", exp: 360,
-  name: "sadida",lvl: 7, characteristic: "hola", exp: 300,
-  name: "sramsito",lvl: 4, characteristic: "hola", exp: 70,
-  name: "zobal",lvl: 4, characteristic: "hola", exp: 70,
-  name: "sacrogrito",lvl: 2, characteristic: "hola", exp: 40,
-  name: "xelotrop",lvl: 3, characteristic: "hola", exp: 55,
-
+Character.transaction do
+  Character.create([
+  {name: "zurcaritas",lvl: 5, characteristic: "hola", exp: 100   },
+  {name: "iop",lvl: 1, characteristic: "probando", exp: 0        },
+  {name: "steamer",lvl: 1, characteristic: "hola", exp: 0        },
+  {name: "timador",lvl: 6, characteristic: "hola", exp: 150      },
+  {name: "sram",lvl: 8, characteristic: "hola", exp: 360         },
+  {name: "sadida",lvl: 7, characteristic: "hola", exp: 300       },
+  {name: "sramsito",lvl: 4, characteristic: "hola", exp: 70      },
+  {name: "zobal",lvl: 4, characteristic: "hola", exp: 70         },
+  {name: "sacrogrito",lvl: 2, characteristic: "hola", exp: 40    },
+  {name: "xelotrop",lvl: 3, characteristic: "hola", exp: 55      },
 ])
+end
+
