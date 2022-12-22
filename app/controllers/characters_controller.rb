a12e7a0663904ef24e71a3f8919e7f865495983c
quetzo <hojitabrillante@gmail.com>
2022-12-22 13:41:36 -0600

    mandando datos del formulario a la vista create, con la ruta post

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,9 +1,15 @@
 class CharactersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @all_character = Character.all # Character.new(datos...)
   end
   def new
     #hola
+  end
+
+  def create
+    @datos = params
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "characters", to: "characters#index" 
+  post "characters", to: "characters#create" 
+
   get "characters/new", to: "characters#new" 
 
   # Defines the root path route ("/")
